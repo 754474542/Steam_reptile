56a4854ae953a2ab0f0557f44bb92666b92038ee
huxian <754474542@qq.com>
2023-06-12 11:35:49 +0800

    遇到跨域问题暂且搁置

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
+$(function () {
+
+    axios({
+        url:'https://setu.yuban10703.xyz/setu',
+        method:'get',
+        params:{
+            r18:0,
+            num:1,
+            replace_url:'https://pixiv.yuki.sh'
+        }
+    }).then(function (res){
+        console.log(res)
+    })
+
+
+
+
+})
 
 
 
